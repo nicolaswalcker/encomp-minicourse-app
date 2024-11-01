eea9959c389f68c5022385113278a029422c9db1
Nicolas Walcker <walcker_@outlook.com>
2024-10-31 22:34:03 -0300

    configure prettier and eslint

---
+++ .eslintrc.cjs
@@ -0,0 +1,31 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: 'standard',
+  overrides: [
+    {
+      env: {
+        node: true
+      },
+      files: [
+        '.eslintrc.{js,cjs}'
+      ],
+      parserOptions: {
+        sourceType: 'script'
+      }
+    }
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  ignorePatterns: [
+    'node_modules/',
+    'dist/',
+    'package.json'
+  ],
+  rules: {
+  }
+}
--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 import './style.css'
 import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+import viteLogo from './public/vite.svg'
+import { setupCounter } from './counter'
 
 document.querySelector('#app').innerHTML = `
   <div>
